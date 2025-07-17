386cca44e63c600fd3aac2ca41d97d2528d97839
HeloisaJ <heloisananda21@gmail.com>
2025-07-17 13:17:38 -0300

    Generated a dockerfile to be the server, the container is working and showing the RViz, however the UR5 is not loading as expected

--- scripts/init_server.sh
+++ scripts/init_server.sh
@@ -5,11 +5,12 @@
 #-----------------------------------------------------
 docker run --rm -it --security-opt seccomp=unconfined \
     --shm-size=512m \
+    --privileged \
     --net ursim_net \
     --ip 192.168.56.100 \
-    --name ros2_cl \
+    --name ros2_sv \
     -e DISPLAY=$DISPLAY \
     -e QT_X11_NO_MITSHM=1 \
     -e ROS_DOMAIN_ID=42 \
     -v /tmp/.X11-unix:/tmp/.X11-unix:rw \
-    test_ros2:latest
+    ros2_sv:latest
