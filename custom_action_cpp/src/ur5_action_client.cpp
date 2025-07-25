9a8fa53529783fbec3dd0f4b01c72da7129fc003
HeloisaJ <heloisananda21@gmail.com>
2025-07-25 09:32:15 -0300

    Generated a initial version of the action client and changed one of the result values for counting delay client to server

--- custom_action_cpp/src/ur5_action_client.cpp
+++ custom_action_cpp/src/ur5_action_client.cpp
@@ -3,12 +3,15 @@
 #include <memory>
 #include <string>
 #include <sstream>
+#include <iostream>
 
 #include "custom_action_interfaces/action/ur5.hpp"
 
 #include "rclcpp/rclcpp.hpp"
 #include "rclcpp_action/rclcpp_action.hpp"
 #include "rclcpp_components/register_node_macro.hpp"
+
+#include "std_msgs/msg/string.hpp"
 
 namespace custom_action_cpp
 {
@@ -22,9 +25,7 @@ namespace custom_action_cpp
 
         rclcpp::Time goal_sent;
 
-        explicit UR5ActionClient(const rclcpp::NodeOptions & options)
-        : Node("ur5_action_client", options)
-        {
+        explicit UR5ActionClient(const rclcpp::NodeOptions & options): Node("ur5_action_client", options){
             this->client_ptr_ = rclcpp_action::create_client<UR5>(
             this,
             "ur5_move");
@@ -63,8 +64,7 @@ namespace custom_action_cpp
                     this->publisher_->publish(message);
 
                     std::stringstream ss;
-                    ss << "Goal accepted by server, goal sent";
-                    ss << goal_handle->execute;
+                    ss << "Goal accepted by server, goal sent" << goal_handle->execute;
                     RCLCPP_INFO(this->get_logger(), ss.str().c_str());
                 }
             };
@@ -74,8 +74,7 @@ namespace custom_action_cpp
             const std::shared_ptr<const UR5::Feedback> feedback)
             {
                 std::stringstream ss;
-                ss << "Status: ";
-                ss << feedback->status;
+                ss << "Status: " << feedback->status << "\n";
                 RCLCPP_INFO(this->get_logger(), ss.str().c_str());
             };
 
@@ -94,12 +93,19 @@ namespace custom_action_cpp
                     RCLCPP_ERROR(this->get_logger(), "Unknown result code");
                     return;
                 }
+
+                std::stringstream ss;
+                ss << "Duration of Arm Movement: " << result.result->duration_move << "\n";
+                ss << "Duration between Goal Received and Start Movement: " << result.result->duration_goal_move << "\n";
+                ss << "Command Delay Server to Client: " << result.result->command_delay << "\n";
             }
         }
-        
+
         private:
         rclcpp_action::Client<UR5>::SharedPtr client_ptr_;
         rclcpp::TimerBase::SharedPtr timer_;
         rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
-    };
-}
+    }; // class UR5ActionClient
+} // namespace custom_action_cpp
+
+RCLCPP_COMPONENTS_REGISTER_NODE(custom_action_cpp::UR5ActionClient)
